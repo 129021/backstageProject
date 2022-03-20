ff4ca4f2dbe80b72eaecf48defba53ef88e30526
129021 <wangjianqin1@yahoo.com>
2022-03-20 10:14:34 +0800

    This project is temporarily closed

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,20 +1,23 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <!-- <div class="dashboard-text">name: {{ name }}</div> -->
+    <div>
+      <h3>
+        首页主要是对echarts的应用，这里先暂时不复习，等待有时间再复习这一部分
+      </h3>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'Dashboard',
+  name: "Dashboard",
   computed: {
-    ...mapGetters([
-      'name'
-    ])
-  }
-}
+    ...mapGetters(["name"]),
+  },
+};
 </script>
 
 <style lang="scss" scoped>
